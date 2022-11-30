71314192274f46766d629a3764146c5491ca185a
Yuta Seto <setoon47@yahoo.co.jp>
2022-11-30 10:35:32 +0900

    青信号のタイマーを定義

---
+++ TrafficLight.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class Alarm{
+    var timer: Timer?
+    var count: Int = 0
+    var limit: Int = 5
+    
+
+    
+    func start(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(blueLightCountup), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(blueLightCountup), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(blueLightCountup), userInfo: nil, repeats: true)
+    }
+    
+    @objc func blueLightCountup(){
+        count += 1
+        print("青信号のカウントは\(count)です")
+        if limit <= count {
+            print("ジリリリ！（青信号のカウントをストップします）")
+            timer?.invalidate()
+        }
+    }
+    
+}
+
+let alarm = Alarm()
+alarm.start()
