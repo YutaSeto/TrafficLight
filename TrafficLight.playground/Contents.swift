51b80bdbf95cb31695c79937189d59c22dce8e89
Yuta Seto <setoon47@yahoo.co.jp>
2022-11-30 10:42:25 +0900

    変数名を修正してコンパイルエラーを解消

--- TrafficLight.playground/Contents.swift
+++ TrafficLight.playground/Contents.swift
@@ -1,22 +1,28 @@
 import UIKit
 
 class Alarm{
-    var timer: Timer?
-    var count: Int = 0
-    var limit: Int = 5
+    var blueTimer: Timer?
+    var blueCount: Int = 0
+    var blueLimit: Int = 5
     
-
+    var yellowTimer: Timer?
+    var yellowCount: Int = 0
+    var yellowLimit: Int = 5
     
-    func start(){
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(blueLightCountup), userInfo: nil, repeats: true)
+    var redTimer: Timer?
+    var redCount: Int = 0
+    var redLimit: Int = 5
+    
+    func blueStart(){
+        blueTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(blueLightCountup), userInfo: nil, repeats: true)
     }
     
     @objc func blueLightCountup(){
-        count += 1
-        print("青信号のカウントは\(count)です")
-        if limit <= count {
+        blueCount += 1
+        print("青信号のカウントは\(blueCount)です")
+        if blueLimit <= blueCount {
             print("ジリリリ！（青信号のカウントをストップします）")
-            timer?.invalidate()
+            blueTimer?.invalidate()
         }
     }
     
